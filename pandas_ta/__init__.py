3c3d6e60c92a026b8c88cd49717f1f0f64400d40
unknown <you@example.com>
2025-02-19 00:56:37 +0530

    first commit

--- pandas_ta/__init__.py
+++ pandas_ta/__init__.py
@@ -4,20 +4,20 @@ name = "pandas_ta"
 """
 from importlib.util import find_spec
 from pathlib import Path
-from pkg_resources import get_distribution, DistributionNotFound
+#from pkg_resources import get_distribution, DistributionNotFound
 
 
-_dist = get_distribution("pandas_ta")
-try:
-    # Normalize case for Windows systems
-    here = Path(_dist.location) / __file__
-    if not here.exists():
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = "Please install this project with setup.py"
+# _dist = get_distribution("pandas_ta")
+# try:
+#     # Normalize case for Windows systems
+#     here = Path(_dist.location) / __file__
+#     if not here.exists():
+#         # not installed, but there is another version that *is*
+#         raise DistributionNotFound
+# except DistributionNotFound:
+#     __version__ = "Please install this project with setup.py"
 
-version = __version__ = _dist.version
+# version = __version__ = _dist.version
 
 Imports = {
     "alphaVantage-api": find_spec("alphaVantageAPI") is not None,
